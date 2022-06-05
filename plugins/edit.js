5afd1ec73546c7d820b1416656dcaebd0b05cf1f
helado de brownie <helado.de.brownie@gmail.com>
2022-06-05 02:00:46 -0400

    update edit command docs to describe new behavior

--- plugins/edit.js
+++ plugins/edit.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'edit',
     usage: 'message:message ...newContent',
-    synopsis: 'Edit a bot messsage, then delete the command message.',
+    synopsis: 'Edit a bot messsage.',
     description:
-`Given the ID or URI of a message in the same channel as the command that was originally posted by the bot, the bot replaces its contents with the specified text.
+`Given the ID or URI of a message that was originally posted by the bot, the bot replaces its contents with the specified text.
 **Note:** The resulting message cannot be the full size of the Discord message limit because of the leading characters in the command string.`,
     async run({message, newContent}) {
         await message.edit(newContent)
