fe7c94e5dba39dedd0558ce6dced47d6f71aa525
helado de brownie <helado.de.brownie@gmail.com>
2022-08-07 00:41:22 -0400

    allow post and edit to copy existing messages

--- plugins/edit.js
+++ plugins/edit.js
@@ -1,33 +1,47 @@
 const fetch = require('../fetch')
 
+const run = async ({messageToEdit, newContent, messageToCopy}, message) => {
+    if (!messageToEdit.editable) {
+        message.reply('I am unable to edit that message.')
+        return
+    }
+
+    // Content is from another message.
+    if (messageToCopy !== undefined) {
+        await messageToEdit.edit(messageToCopy.content)
+    // Content is from the command message.
+    } else if (newContent !== undefined) {
+        await messageToEdit.edit(newContent)
+    // Content is from an attachment.
+    } else {
+        if (message.attachments.size !== 1) {
+            message.reply('Please attach precisely one attachment.')
+            return
+        }
+
+        const attachment = message.attachments.first()
+        const attachedContent = await fetch(attachment.url)
+
+        if (attachedContent.length > 2000) {
+            await message.reply('The attached content must not exceed the Discord message length limit of 2000 characters.')
+        } else {
+            await messageToEdit.edit(attachedContent)
+        }
+    }
+}
+
 module.exports = {
     name: 'edit',
     usage: [
         'messageToEdit:message',
-        'messageToEdit:message ...newContent'
+        'messageToEdit:message "to" "match" messageToCopy:message',
+        'messageToEdit:message "text" ...newContent',
     ],
     synopsis: 'Edit a bot messsage.',
     description:
-`Given a message that was originally posted by the bot, the bot replaces its contents with the specified text.
-If the content isn't specified as part of the command text, it's taken from an attachment instead. Make sure there is exactly one attachment and that it's a text file.
-**Note:** Although the bot itself doesn't ping any users mentioned in the command output, you will still end up pinging them in the message to run the command unless you use the aforementioned attachment mode.`,
-    async run({messageToEdit, newContent}, message) {
-        if (newContent === undefined) {
-            if (message.attachments.size !== 1) {
-                message.reply('Please attach precisely one attachment.')
-                return
-            }
-
-            const attachment = message.attachments.first()
-            const attachedContent = await fetch(attachment.url)
-
-            if (attachedContent.length > 2000) {
-                await message.reply('The attached content must not exceed the Discord message length limit of 2000 characters.')
-            } else {
-                await messageToEdit.edit(attachedContent)
-            }
-        } else {
-            await messageToEdit.edit(newContent)
-        }
-    },
+`Given a message that was originally posted by the bot, the bot replaces its content with the specified text.
+The first form, with no arguments besides the message, takes the content from an attached file. Make sure there is exactly one attachment and that it's a text file.
+The second form, introduced with the words \`to match\`, takes the content from another specified message.
+The third form, introduced with the word \`text\`, takes the content from the message that triggered the command. (**Note:** Although the bot itself doesn't ping any users mentioned in the command output, you will still end up pinging them in the message to run the command if you use this form.)`,
+    run,
 }
--- plugins/post.js
+++ plugins/post.js
@@ -1,33 +1,42 @@
 const fetch = require('../fetch')
 
+const run = async ({channel, content, messageToCopy}, message) => {
+    // Content is from another message.
+    if (messageToCopy !== undefined) {
+        await channel.send(messageToCopy.content)
+    // Content is from the command message.
+    } else if (content !== undefined) {
+        await channel.send(content)
+    // Content is from an attachment.
+    } else {
+        if (message.attachments.size !== 1) {
+            message.reply('Please attach precisely one attachment.')
+            return
+        }
+
+        const attachment = message.attachments.first()
+        const attachedContent = await fetch(attachment.url)
+
+        if (attachedContent.length > 2000) {
+            await message.reply('The attached content must not exceed the Discord message length limit of 2000 characters.')
+        } else {
+            await channel.send(attachedContent)
+        }
+    }
+}
+
 module.exports = {
     name: 'post',
     usage: [
         'channel:channel',
-        'channel:channel ...content',
+        'messageToCopy:message "to" channel:channel',
+        'channel:channel "text" ...content',
     ],
     synopsis: 'Send a message.',
     description:
 `Post the given content in the given channel.
-If the content isn't specified as part of the command text, it's taken from an attachment instead. Make sure there is exactly one attachment and that it's a text file.
-**Note:** Although the bot itself doesn't ping any users mentioned in the command output, you will still end up pinging them in the message to run the command unless you use the aforementioned attachment mode.`,
-    async run({channel, content}, message) {
-        if (content === undefined) {
-            if (message.attachments.size !== 1) {
-                message.reply('Please attach precisely one attachment.')
-                return
-            }
-
-            const attachment = message.attachments.first()
-            const attachedContent = await fetch(attachment.url)
-
-            if (attachedContent.length > 2000) {
-                await message.reply('The attached content must not exceed the Discord message length limit of 2000 characters.')
-            } else {
-                await channel.send(attachedContent)
-            }
-        } else {
-            await channel.send(content)
-        }
-    },
+The first form, with no arguments besides the message, takes the content from an attached file. Make sure there is exactly one attachment and that it's a text file.
+The second form, taking a message, the word \`to\`, and a channel, takes the content from a specified message.
+The third form, introduced with the word \`text\`, takes the content from the message that triggered the command. (**Note:** Although the bot itself doesn't ping any users mentioned in the command output, you will still end up pinging them in the message to run the command if you use this form.)`,
+    run,
 }
